2b2d9bd79d614e78ded64d0683f29323608eb8e3
Slaimon <simo.bertolucci@gmail.com>
2026-05-07 03:21:07 +0200

    Change the digits' svg

--- clock.js
+++ clock.js
@@ -1,16 +1,26 @@
 export {Clock};
 
 const digit = (place) => 
-`<svg class="digit" id="digit_${place}" fill="currentColor" width="153.46mm" height="153.52mm" version="1.1" viewBox="0 0 153.46 153.52" xmlns="http://www.w3.org/2000/svg">
-    <g transform="translate(-59.531 -149.43)" stroke-width="0">
-    <path class="segment s_b" d="m150.81 150.81s-8.8191-1e-3 -8.8202 2e-3l-50.711 152.13h13.229l46.302-138.91z"/>
-    <path class="segment s_d" d="m153.46 150.81h8.8194l50.712 152.14h-13.229l-46.302-138.91z"/>
-    <path class="segment s_a" d="m152.14 235.48h-23.812l-2.6458 7.9375h52.917l-2.6458-7.9375z"/>
-    <path class="segment s_a" d="m85.99 243.42h-23.812l-2.6458-7.9375h52.917l-2.6458 7.9375z"/>
-    <path class="segment s_c" d="m70.031 150.81c-7.321-12.465 37.113 63.193 45.063 76.729l-2.2049 6.6146h-5.7326l-47.625-83.344z"/>
-    <path class="segment s_e" d="m153.34 302.95-30.304-51.599 2.2049-6.6146h5.7326l33.073 58.208z"/>
-    </g>
+`<svg class="digit" id="digit_${place}" fill="currentColor" viewBox="0 0 36 48" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <g transform="translate(-59.53125 -149.48952)">
+    <path class="segment s_a" d="m 64.029167,171.44994 v 3.70417 h 9.122637 l 2.525567,-3.70417 z"/>
+    <path class="segment s_a" d="m 79.103879,175.15411 2.525568,-3.70417 h 9.122638 v 3.70417 z"/>
+    <path class="segment s_b" d="m 59.531251,196.05618 v 1.11648 h 3.929109 l 31.789645,-46.62482 -6e-6,-1.00018 h -4.008392 z"/>
+    <path class="segment s_f" d="m 78.5458,175.97262 -2.481463,3.63948 10.138596,17.56056 h 4.582723 L 78.5458,175.97262"/>
+    <path class="segment s_c" d="m 76.235451,170.63142 h 0 l 2.481463,-3.63948 -10.071462,-17.44428 h -4.582716 z"/>
+    <path class="segment s_d" d="M 59.531251,149.54766 H 63.5 v 39.76242 l -3.968749,5.80379 0,0.017 0,-45.58325"/>
+    <path class="segment s_e" d="m 91.281248,157.29397 3.96875,-5.82084 v 45.69953 h -3.96875 z"/>
+  </g>
 </svg>`;
+
+const segments = {
+    0: ['a'],
+    1: ['b'],
+    2: ['b','c'],
+    3: ['b','d','e'],
+    4: ['b','f'],
+    5: ['b','e','f']
+}
 
 class Clock {
     constructor(parent, num_digits, num = 0) {
@@ -86,15 +96,6 @@ class Clock {
             digit_display(this.digits[i], parseInt(num_string[i]));
         }
     }
-}
-
-const segments = {
-    0: ['a'],
-    1: ['b'],
-    2: ['b','c'],
-    3: ['b','c','d'],
-    4: ['b','e'],
-    5: ['b','d','e']
 }
 
 function activate_segments(digit, list) {
